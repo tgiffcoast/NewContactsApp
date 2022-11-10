515f111efbcdb257dc3ca06bce41c5df4988bd94
Tracey Giffney <traceygiffney@hotmail.com>
2022-11-10 19:22:25 +1100

    Commit101122 722pm

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tracey Testing hi hi hi !</Text>
+      <Text>need commits to workfgfdgfdgfgfgd to remote !</Text>
+      <Text>I want to test more go parra !</Text>
       <StatusBar style="auto" />
     </View>
   );
