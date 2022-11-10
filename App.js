a35ad7530f1fd03f4f0bc3defac8a52047d39c4e
Tracey Giffney <traceygiffney@hotmail.com>
2022-11-10 19:35:48 +1100

    I like committing

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tracey Testing hi hi hi !</Text>
-      <Text>need commits tofghfghfgh workfgfdgfdgfgfgd to remote !</Text>
-      <Text>I want to test more go parra !</Text>
+      <Text>I want to test more 734pm !</Text>
       <StatusBar style="auto" />
     </View>
   );
